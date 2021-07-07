4ef59e82cc7c7a80b48eb1646b1a74f1cefee352
Apongpoh <83125905+Apongpoh@users.noreply.github.com>
2021-07-08 00:33:16 +0100

    Update index.js

--- frontend/src/auth/index.js
+++ frontend/src/auth/index.js
@@ -1,1 +1,51 @@
 
+import { API } from '../config';
+
+export const authenticate = (data, next) => {
+    if (typeof window !== 'undefined') {
+        localStorage.setItem('jwt', JSON.stringify(data));
+        next();
+    }
+};
+
+export const signin = user => {
+    return fetch(`${API}/signin`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err)
+        });
+};
+
+export const signout = next => {
+    if (typeof window !== 'undefined') {
+        localStorage.removeItem('jwt');
+        next();
+        return fetch(`${API}/signout`, {
+            method: 'GET'
+        })
+            .then(response => {
+                console.log('signout', response);
+            })
+            .catch(err => console.log(err));
+    }
+};
+
+export const isAuthenticated = () => {
+    if (typeof window == 'undefined') {
+        return false;
+    }
+    if (localStorage.getItem('jwt')) {
+        return JSON.parse(localStorage.getItem('jwt'));
+    } else {
+        return false;
+    }
+};
